64b198ea37682737600b2e865f1b7bd73bb42550
Andreas Thomas <dev@chronark.com>
2021-05-27 16:33:05 +0200

    feat: split auth pages in the center (#11)

--- app/auth/components/template.tsx
+++ app/auth/components/template.tsx
@@ -19,7 +19,7 @@ export const AuthPageTemplate: React.FC<AuthPageTemplateProps> = ({
         </span>
 
         <div className="flex flex-col items-center justify-center h-full space-y-10 md:space-y-0 md:flex-row">
-          <div className="w-full md:h-full md:bg-gradient-to-r md:from-white md:via-white md:to-gray-100">
+          <div className="w-full md:w-1/2 md:h-full md:bg-gradient-to-r md:from-white md:via-white md:to-gray-100">
             <div className="flex flex-col items-center justify-center w-full md:h-full">
               <div className="flex flex-col items-center justify-center space-y-4 tracking-tight md:items-start md:justify-start lg:max-w-3xl">
                 <h1 className="text-5xl font-bold text-center text-gray-900 xl:text-6xl">{h1}</h1>
@@ -28,9 +28,7 @@ export const AuthPageTemplate: React.FC<AuthPageTemplateProps> = ({
             </div>
           </div>
 
-          <div className="flex items-center justify-center space-y-16 md:max-w-sm md:w-full">
-            {children}
-          </div>
+          <div className="flex items-center justify-center space-y-16 md:w-1/2">{children}</div>
         </div>
       </div>
     </section>
