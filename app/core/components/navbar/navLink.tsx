a80028ed2a721cc3bebac01a6fdb5dc96df0d94f
Andreas Thomas <dev@chronark.com>
2021-05-29 00:10:59 +0200

    design (#21)
    * feat: add sentry
    
    * feat: finish auth page design
    
    * chore: merge
    
    Co-authored-by: Andreas Thomas <chronark@pop-os.localdomain>

--- app/core/components/navbar/navLink.tsx
+++ app/core/components/navbar/navLink.tsx
@@ -8,7 +8,7 @@ export interface NavLinkProps {
 
 export const NavLink: React.FC<NavLinkProps> = ({ label, prefix, postfix }): JSX.Element => {
   return (
-    <div className="inline-flex items-center px-3 py-2 space-x-2 text-base font-medium text-gray-200 group text-opacity-90 hover:text-opacity-100 focus:outline-none">
+    <div className="inline-flex items-center px-3 py-2 space-x-2 text-base font-medium text-gray-200 group hover:text-gray-50 focus:outline-none">
       <div className="w-5 h-">{prefix}</div>
       <span>{label}</span>
       <div className="w-5 h-5">{postfix}</div>
