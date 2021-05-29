e008c5a463207472c814a1a5fce12ab5698c9a6a
Andreas Thomas <dev@chronark.com>
2021-05-29 12:59:39 +0200

    health check (#25)
    * feat: add sentry
    
    * chore: sync lock file
    
    * style: fmt
    
    * feat: add health check api endpoint
    
    Co-authored-by: Andreas Thomas <chronark@pop-os.localdomain>

---
+++ app/api/health.ts
@@ -0,0 +1,9 @@
+
+import {BlitzApiRequest, BlitzApiResponse} from "blitz"
+
+const handler = (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "application/json")
+  res.end(JSON.stringify({ status: "ok" }))
+}
+export default handler
