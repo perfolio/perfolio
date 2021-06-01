ed9e621f22e7d71960a019872bcda537090b2a7a
Andreas Thomas <dev@chronark.com>
2021-06-01 18:00:16 +0200

    feat: use next-image everywhere (#37)
    * feat: use next-image everywhere
    
    * feat: use next-image everywhere

--- blitz.config.ts
+++ blitz.config.ts
@@ -7,6 +7,9 @@ module.exports = {
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
