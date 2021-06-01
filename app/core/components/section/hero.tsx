c24a82fdaca1c562caf0e9c4350ee1cec08311b2
Andreas Thomas <dev@chronark.com>
2021-06-01 21:55:07 +0200

    fix: landing page mobile layout

--- app/core/components/section/hero.tsx
+++ app/core/components/section/hero.tsx
@@ -1,31 +1,31 @@
 import React from "react"
+import { Button } from "app/core/components"
 
-export interface HeroSectionProps {
-  headline: string
-  paragraph: React.ReactNode
-  primaryButton: React.ReactNode
-  secondaryButton: React.ReactNode
-
-  children?: React.ReactNode
-}
-
-export const HeroSection: React.FC<HeroSectionProps> = (props): JSX.Element => {
+export const HeroSection: React.FC = (): JSX.Element => {
   return (
-    <div className="flex items-center justify-center w-full max-w-screen-xl">
-      <div className="flex flex-col justify-center space-y-4 text-center md:space-y-8 xl:space-y-12 ">
-        <h1 className="py-4 -my-4 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-tr from-gray-900 to-primary-700 text-shadow-md sm:text-5xl lg:text-6xl">
-          {props.headline}
-        </h1>
-        <p className="text-gray-600 sm:text-lg sm:mx-auto md:text-xl lg:mx-0">
-          {props.paragraph}
-        </p>
+    <div className="flex flex-col items-center w-full space-y-4 md:space-y-8 xl:space-y-12 ">
+      <h1 className="text-3xl font-extrabold text-transparent sm:text-center bg-clip-text bg-gradient-to-tr from-gray-900 to-primary-700 text-shadow-md sm:text-5xl lg:text-6xl">
+        Insights. <br className="sm:hidden"></br> For Everyone.
+      </h1>
+      <p className="text-gray-600 sm:text-lg sm:mx-auto md:text-xl lg:mx-0 sm:text-center">
+        Keeping track of all your assets and their performance is hard. Perfolio
+        brings all information to one place and gives you access to the latest
+        analytics methods in science.
+      </p>
 
-        <div className="space-y-4 md:space-x-4 md:space-y-0">
-          <div className="items-center justify-center sm:flex ">
-            <div className="">{props.primaryButton}</div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">{props.secondaryButton}</div>
-          </div>
-        </div>
+      <div className="grid w-full gap-4 sm:w-1/2 md:w-1/3 lg:w-1/4 sm:grid-cols-2">
+        <Button
+          kind="primary"
+          size="auto"
+          label="Sign up"
+          href="/auth/signup"
+        />
+        <Button
+          size="auto"
+          kind="secondary"
+          label="Contact"
+          href="mailto:info@perfol.io"
+        />
       </div>
     </div>
   )
