225b99130aba074d6b25d17394556e652130e412
Andreas Thomas <dev@chronark.com>
2021-06-02 12:18:06 +0200

    lhci (#53)
    * feat: add terraform
    
    * fix: resource names
    
    * style: fmt
    
    * chore: first migration
    
    * fix: broken links and rename to signin
    
    * feat: add lhci assertions
    
    * feat: add lhci default assertions
    
    * fix: prisma command
    
    * ci: run lhci in parallel
    
    * ci: add github token

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,22 +1,20 @@
 module.exports = {
   ci: {
     collect: {
-      url: [
-        "http://localhost:3000/",
-        "http://localhost:3000/imprint",
-        "http://localhost:3000/privacy",
-        "http://localhost:3000/signin",
-        "http://localhost:3000/signup",
-        "http://localhost:3000/subscribe",
-        "http://localhost:3000/forgot-password",
-        "http://localhost:3000/reset-password",
-      ],
       startServerCommand: "yarn start",
       startServerReadyTimeout: 3 * 60 * 1000,
-      // startServerReadyPattern: "started server on 0.0.0.0:3000"
     },
     upload: {
       target: "temporary-public-storage",
+    },
+  },
+  assert: {
+    preset: "lighthouse:recommend",
+    assertions: {
+      "categories:performance": ["error", { minScore: 0.9 }],
+      "categories:accessibility": ["error", { minScore: 0.9 }],
+      "categories:best-practices": ["error", { minScore: 0.9 }],
+      "categories:seo": ["error", { minScore: 0.9 }],
     },
   },
 }
