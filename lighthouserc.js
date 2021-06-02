1b2c9683ab6c6c7495b3f43ad9710e748a083fa2
Andreas Thomas <dev@chronark.com>
2021-06-02 13:18:58 +0200

    ci: refactor wait-for-vercel

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,10 +7,10 @@ module.exports = {
   assert: {
     preset: "lighthouse:recommend",
     assertions: {
-      "categories:performance": ["error", { minScore: 0.9 }],
-      "categories:accessibility": ["error", { minScore: 0.9 }],
-      "categories:best-practices": ["error", { minScore: 0.9 }],
-      "categories:seo": ["error", { minScore: 0.9 }],
+      "categories:performance": ["error", { minScore: 0.95 }],
+      "categories:accessibility": ["error", { minScore: 0.95 }],
+      "categories:best-practices": ["error", { minScore: 0.95 }],
+      "categories:seo": ["error", { minScore: 0.95 }],
     },
   },
 }
