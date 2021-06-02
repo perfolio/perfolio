b4580abf8953b0162e21a57af101b93fe01ad2c4
Andreas Thomas <dev@chronark.com>
2021-06-02 12:07:12 +0200

    ci: run lhci in parallel

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,6 @@
 module.exports = {
   ci: {
     collect: {
-      url: [
-        "http://localhost:3000/",
-        "http://localhost:3000/imprint",
-        "http://localhost:3000/privacy",
-        "http://localhost:3000/signin",
-        "http://localhost:3000/signup",
-        "http://localhost:3000/subscribe",
-        "http://localhost:3000/forgot-password",
-        "http://localhost:3000/reset-password",
-      ],
       startServerCommand: "yarn start",
       startServerReadyTimeout: 3 * 60 * 1000,
     },
