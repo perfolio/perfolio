6e7e349fae62bf36611360997968d5bbe6ba2a92
Andreas Thomas <dev@chronark.com>
2021-06-02 14:15:30 +0200

    ci: actually use config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,14 +3,13 @@ module.exports = {
     upload: {
       target: "temporary-public-storage",
     },
-  },
-  assert: {
-    preset: "lighthouse:recommend",
-    assertions: {
-      "categories:performance": ["error", { minScore: 0.95 }],
-      "categories:accessibility": ["error", { minScore: 0.95 }],
-      "categories:best-practices": ["error", { minScore: 0.95 }],
-      "categories:seo": ["error", { minScore: 0.95 }],
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.95 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.95 }],
+      },
     },
   },
 }
