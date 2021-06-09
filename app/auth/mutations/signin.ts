d624b5317f24929d018cf1861e107ae0b00e8462
Andreas Thomas <dev@chronark.com>
2021-06-09 13:26:09 +0200

    fix: allow signin

--- app/auth/mutations/signin.ts
+++ app/auth/mutations/signin.ts
@@ -6,7 +6,6 @@ export default resolver.pipe(
   resolver.zod(Signin),
   async ({ email, password }, ctx) => {
     const user = await db.user.signin({ email, password })
-    console.log({ user })
     await ctx.session.$create({ userId: user.id, role: "USER" })
     return user
   },
