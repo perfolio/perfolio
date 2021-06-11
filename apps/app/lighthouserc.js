38d0081c8a83103bc2f43b267b1573ae12339ed2
Andreas Thomas <dev@chronark.com>
2021-06-11 17:31:41 +0200

    feat: wip

--- apps/app/lighthouserc.js
+++ apps/app/lighthouserc.js
@@ -9,11 +9,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        "is-crawlable": ["off"],
-        "errors-in-console": ["off"],
-        "no-vulnerable-libraries": ["off"],
-        "non-composited-animations": ["off"],
-        "categories:accessibility": ["error", { minScore: 0.7 }],
+        "categories:accessibility": ["error", { minScore: 0.5 }],
         "categories:best-practices": ["error", { minScore: 0.7 }],
         "categories:seo": ["error", { minScore: 0.7 }],
       },
