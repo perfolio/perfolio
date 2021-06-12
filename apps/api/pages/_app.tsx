387b8af08e3103bb9caca43dc615681135709cc2
Andreas Thomas <dev@chronark.com>
2021-06-13 00:30:02 +0200

    dedicated api (#122)
    * refactor: move api to api-client
    
    * feat: add api application
    
    * feat: redirect /api to /
    
    * feat: redirect /api to /
    
    * feat: redirect /api to /
    
    * feat: add dummy api route
    
    * feat: add dummy api route
    
    * feat: add dummy api route
    
    * feat: redirect from /v1 to /api/v1

--- apps/api/pages/_app.tsx
+++ apps/api/pages/_app.tsx
@@ -1,26 +1,8 @@
 import React from "react"
 import { AppProps } from "next/app"
-import Head from "next/head"
-import { ReactComponent as NxLogo } from "../public/nx-logo-white.svg"
-import "./styles.css"
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to api!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to api!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default CustomApp
