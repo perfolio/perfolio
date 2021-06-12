b27b78e67087a3061d247f4f5f208b3796a19a2c
Andreas Thomas <dev@chronark.com>
2021-06-12 23:07:31 +0200

    feat: redirect /api to /

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -10,10 +10,24 @@ module.exports = withNx({
   async redirects() {
     return [
       {
-        source: '/api/*',
-        destination: '/*',
+        source: '/api/:path*',
+        destination: '/:path*',
         permanent: true,
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "OPTIONS,POST" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 })
