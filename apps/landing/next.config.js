7c7727b4e857284b88285623a542a29c02718683
Andreas Thomas <dev@chronark.com>
2021-06-12 14:08:28 +0200

    feat: add sentry, not sure of it works

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,10 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable @typescript-eslint/no-var-requires
 const withNx = require("@nrwl/next/plugins/with-nx")
+const { withSentryConfig } = require("@sentry/nextjs")
 
-module.exports = withNx({
-  nx: {
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
-})
+module.exports = withNx(
+  withSentryConfig(
+    {
+      nx: {
+        svgr: true,
+      },
+    },
+    {},
+  ),
+)
