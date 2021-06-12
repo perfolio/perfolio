8dd54631c043879bb467bae82bd4f0bd868954b0
Andreas Thomas <dev@chronark.com>
2021-06-13 00:24:25 +0200

    feat: redirect from /v1 to /api/v1

--- apps/api/next.config.js
+++ apps/api/next.config.js
@@ -5,28 +5,28 @@ module.exports = withNx({
   async redirects() {
     return [
       {
-        source: "/api/:path*",
-        destination: "/:path*",
+        source: "/:version(v\\d+)/:slug*",
+        destination: "/api/:version/:slug*",
         permanent: true,
       },
     ]
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "OPTIONS,POST" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,POST" },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value:
+  //             "X-CSRF-Token, X-Requested-With, Accept, Authorization, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 })
