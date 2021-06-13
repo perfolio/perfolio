6245272609cd22b5bbd6bc22142bffaa05b0937a
Andreas Thomas <dev@chronark.com>
2021-06-13 21:41:51 +0200

    feat: send 200 for options requests

--- apps/api/pages/api/v1/auth/signout.ts
+++ apps/api/pages/api/v1/auth/signout.ts
@@ -1,3 +1,3 @@
-import { withContentTypeJson, withAuthentication, use } from "@perfolio/middleware"
+import { withContentTypeJson, withAuthentication, use, withOptions } from "@perfolio/middleware"
 import { signout } from "@perfolio/lambda"
-export default use(signout, [withContentTypeJson, withAuthentication])
+export default use(signout, [withOptions, withContentTypeJson, withAuthentication])
