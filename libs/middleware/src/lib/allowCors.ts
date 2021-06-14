ad4c2ee00e0c0f5a8bc211b5ac16b6dd18a41198
Andreas Thomas <dev@chronark.com>
2021-06-14 19:06:52 +0200

    cors (#159)
    * fix: reverse middleware application
    
    * fix: reverse middleware application
    
    * fix: reverse middleware application
    
    * fix: reverse middleware application
    
    * fix: reverse middleware application
    
    * feat: log options call
    
    * fix: reverse middleware application

--- libs/middleware/src/lib/allowCors.ts
+++ libs/middleware/src/lib/allowCors.ts
@@ -5,9 +5,14 @@ import { MiddlewareContext, ApiHandler } from "./types"
  */
 export function allowCors(handler: ApiHandler): ApiHandler {
   return async (ctx: MiddlewareContext): Promise<void> => {
+    console.log("Applying cors middleware")
+
     ctx.res.setHeader("Access-Control-Allow-Credentials", "true")
     if (ctx.req.headers.origin) {
       ctx.res.setHeader("Access-Control-Allow-Origin", ctx.req.headers.origin)
+      console.log(
+        `Setting Access-Control-Allow-Origin=${ctx.res.getHeader("Access-Control-Allow-Origin")}`,
+      )
     }
     ctx.res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,DELETE,OPTIONS,POST")
     ctx.res.setHeader(
@@ -15,6 +20,7 @@ export function allowCors(handler: ApiHandler): ApiHandler {
       "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     )
     if (ctx.req.method === "OPTIONS") {
+      console.log("detected OPTIONS, returning 200")
       return ctx.res.status(200).end()
     }
 
