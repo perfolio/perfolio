cf347b4f4a0b5aa5554533b716de441a98011e6d
Andreas Thomas <dev@chronark.com>
2021-06-14 22:34:18 +0200

    fix: strip all middleware from signin

--- apps/api/pages/api/v1/auth/signin.ts
+++ apps/api/pages/api/v1/auth/signin.ts
@@ -6,6 +6,14 @@ function handleOptions(handler: ApiHandler): ApiHandler {
     console.log("Method:", ctx.req.method)
     if (ctx.req.method === "OPTIONS") {
       console.log("detected OPTIONS, returning 200")
+      ctx.res.setHeader("Access-Control-Allow-Credentials", "true")
+      ctx.res.setHeader("Access-Control-Allow-Origin", "*")
+      ctx.res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
+      ctx.res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      )
+
       return ctx.res.status(200).end()
     } else {
       return handler(ctx)
