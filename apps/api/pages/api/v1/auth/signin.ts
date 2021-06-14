817411949e033895d78d2a5885717c7697d187c7
Andreas Thomas <dev@chronark.com>
2021-06-14 22:22:06 +0200

    fix: strip all middleware from signin

--- apps/api/pages/api/v1/auth/signin.ts
+++ apps/api/pages/api/v1/auth/signin.ts
@@ -1,4 +1,16 @@
-import { use } from "@perfolio/middleware"
 import { signin } from "@perfolio/lambda"
 
-export default use(signin, [])
+import { use, MiddlewareContext, ApiHandler } from "@perfolio/middleware"
+function handleOptions(handler: ApiHandler): ApiHandler {
+  return async (ctx: MiddlewareContext): Promise<void> => {
+    console.log("Method:", ctx.req.method)
+    if (ctx.req.method === "OPTIONS") {
+      console.log("detected OPTIONS, returning 200")
+      return ctx.res.status(200).end()
+    } else {
+      return handler(ctx)
+    }
+  }
+}
+
+export default use(signin, [handleOptions])
