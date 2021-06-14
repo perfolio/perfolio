097f3c9899b78ffdb9e40d05ce2bf3cd906fcef1
Andreas Thomas <dev@chronark.com>
2021-06-14 22:34:42 +0200

    fix: strip all middleware from signin

--- apps/api/pages/api/v1/auth/signin.ts
+++ apps/api/pages/api/v1/auth/signin.ts
@@ -4,15 +4,15 @@ import { use, MiddlewareContext, ApiHandler } from "@perfolio/middleware"
 function handleOptions(handler: ApiHandler): ApiHandler {
   return async (ctx: MiddlewareContext): Promise<void> => {
     console.log("Method:", ctx.req.method)
+    ctx.res.setHeader("Access-Control-Allow-Credentials", "true")
+    ctx.res.setHeader("Access-Control-Allow-Origin", "*")
+    ctx.res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
+    ctx.res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    )
     if (ctx.req.method === "OPTIONS") {
       console.log("detected OPTIONS, returning 200")
-      ctx.res.setHeader("Access-Control-Allow-Credentials", "true")
-      ctx.res.setHeader("Access-Control-Allow-Origin", "*")
-      ctx.res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
-      ctx.res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-      )
 
       return ctx.res.status(200).end()
     } else {
