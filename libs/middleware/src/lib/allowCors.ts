076941a51b234b3c88d14908d5173395806d1611
Andreas Thomas <dev@chronark.com>
2021-06-14 22:43:05 +0200

    fix: strip all middleware from signin

--- libs/middleware/src/lib/allowCors.ts
+++ libs/middleware/src/lib/allowCors.ts
@@ -5,25 +5,20 @@ import { MiddlewareContext, ApiHandler } from "./types"
  */
 export function allowCors(handler: ApiHandler): ApiHandler {
   return async (ctx: MiddlewareContext): Promise<void> => {
-    console.log("Applying cors middleware")
-
+    console.log("Method:", ctx.req.method)
     ctx.res.setHeader("Access-Control-Allow-Credentials", "true")
-    if (ctx.req.headers.origin) {
-      ctx.res.setHeader("Access-Control-Allow-Origin", ctx.req.headers.origin)
-      console.log(
-        `Setting Access-Control-Allow-Origin=${ctx.res.getHeader("Access-Control-Allow-Origin")}`,
-      )
-    }
-    ctx.res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,DELETE,OPTIONS,POST")
+    ctx.res.setHeader("Access-Control-Allow-Origin", "*")
+    ctx.res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT")
     ctx.res.setHeader(
       "Access-Control-Allow-Headers",
       "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     )
     if (ctx.req.method === "OPTIONS") {
       console.log("detected OPTIONS, returning 200")
-      return ctx.res.status(200).end()
-    }
 
-    return handler(ctx)
+      return ctx.res.status(200).end()
+    } else {
+      return handler(ctx)
+    }
   }
 }
