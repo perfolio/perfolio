6fca2da4691b23173e41d8026d17ba8b91c873e2
Andreas Thomas <dev@chronark.com>
2021-06-15 19:36:00 +0200

    feat: add sentry

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,10 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require("@nrwl/next/plugins/with-nx")
+const { withSentryConfig } = require("@sentry/nextjs")
 
-module.exports = withNx({
-  nx: {
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
-})
+module.exports = withNx(
+  withSentryConfig(
+    {
+      nx: {
+        // Set this to false if you do not want to use SVGR
+        // See: https://github.com/gregberge/svgr
+        svgr: true,
+      },
+    },
+    {},
+  ),
+)
