2b98ab8a3a34a352f415f174fb595c1a7d628819
Andreas Thomas <dev@chronark.com>
2021-06-15 20:07:49 +0200

    feat: add sentry

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -4,17 +4,11 @@ const { withSentryConfig } = require("@sentry/nextjs")
 
 module.exports = withNx(
   withSentryConfig(
-    {
-      nx: {
-        // Set this to false if you do not want to use SVGR
-        // See: https://github.com/gregberge/svgr
-        svgr: true,
-      },
-    },
+    {},
     {
       org: "chronark",
-      project: "appperfolio",
-      authToken: "cf6c23ce1a7f418fa9d77e7801d01f8944a4a3f30196492ab071df228ac19824",
+      project: "app-perfolio",
+      authToken: process.env.NX_SENTRY_AUTH_TOKEN,
     },
   ),
 )
