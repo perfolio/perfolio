477ec358689a498c7bcb3cc8e58eba9755795dd5
Andreas Thomas <dev@chronark.com>
2021-06-16 15:53:38 +0200

    chore: update checkly version

--- deployments/terraform.tf
+++ deployments/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     checkly = {
       source  = "checkly/checkly"
-      version = "0.8.1"
+      version = "1.1.0"
     }
       cloudflare = {
       source = "cloudflare/cloudflare"
