2addd96fc3f4fd34a4e044189f806a85a98f69fa
Andreas Thomas <dev@chronark.com>
2021-06-22 12:44:08 +0200

    settings (#241)
    * revert: do not redirect
    
    * feat: add company logo to transactions page
    
    * feat: allow user to set name and delete account

---
+++ libs/api/feature/lambda/src/lib/settings/changeName.ts
@@ -0,0 +1,14 @@
+import { MiddlewareContext } from "@perfolio/api/feature/middleware"
+import { db } from "@perfolio/data-access/db"
+import { z } from "zod"
+export const ChangeNameRequestValidation = z.object({
+  name: z.string().min(3).max(64),
+})
+
+export type ChangeNameRequest = z.infer<typeof ChangeNameRequestValidation>
+export async function changeName(
+  { name }: ChangeNameRequest,
+  ctx: MiddlewareContext,
+): Promise<void> {
+  await db().user.update(ctx.claims.userId, { name })
+}
