27108b16b5c90a0dabbecd337ff4a84c20652aa1
Andreas Thomas <dev@chronark.com>
2021-06-23 14:55:25 +0200

    feat: add description component

---
+++ libs/ui/design-system/src/lib/description/description.stories.tsx
@@ -0,0 +1,13 @@
+import { Description } from "./description"
+export default {
+  title: "Data Display/Description",
+  component: Description,
+}
+
+export const Default = () => (
+  <Description title="Section title">
+    To get the most out of this module, you should have worked your way through the previous
+    JavaScript modules in the series. Those modules typically involve simple API usage, as it is
+    often difficult to write client-side JavaScript examples without them.
+  </Description>
+)
