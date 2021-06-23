dee76380820473e66382dc1b83bebdeab686ac2f
Andreas Thomas <dev@chronark.com>
2021-06-23 13:24:00 +0200

    design system (#245)
    * revert: do not redirect
    
    * feat: text component
    
    * feat: text component
    
    * feat: avatar component
    
    * fix: set fetch depth: 0
    
    * fix: set fetch depth: 0
    
    * feat: increase small avatar size

---
+++ libs/ui/design-system/src/lib/general/heading/heading.stories.tsx
@@ -0,0 +1,32 @@
+import { Heading, HeadingProps } from "./heading"
+
+export default {
+  title: "General/Heading",
+  component: Heading,
+}
+
+export const Headings = (args: HeadingProps) => (
+  <div>
+    <Heading {...args} h1>
+      Heading
+    </Heading>
+    <Heading {...args} h2>
+      Heading
+    </Heading>
+    <Heading {...args} h3>
+      Heading
+    </Heading>
+    <Heading {...args} h4>
+      Heading
+    </Heading>
+  </div>
+)
+
+export const CustomColor = (args: HeadingProps) => (
+  <Heading h1 {...args}>
+    Colors
+  </Heading>
+)
+CustomColor.args = {
+  color: "text-error-500",
+}
