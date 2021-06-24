815a300aa5e31743938e427573dd930c1e16af06
Andreas Thomas <dev@chronark.com>
2021-06-24 14:17:15 +0200

    feat: use new logo

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -12,13 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Perfolio</title>
         <meta name="description" content="Insights. For Everyone." />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-        <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png"></link>
         <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png"></link>
         <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png"></link>
-        <link rel="manifest" href="/fav/site.webmanifest"></link>
-        <link rel="mask-icon" href="/fav/safari-pinned-tab.svg" color="#1A202C"></link>
-        <meta name="msapplication-TileColor" content="#1A202C"></meta>
-        <meta name="theme-color" content="#1A202C"></meta>
       </Head>
       <QueryClientProvider client={PersistentQueryClient()}>
         <ApiProvider>
