c70981781cca40e54bc89c9aba49c31b15f393f1
Andreas Thomas <dev@chronark.com>
2021-08-03 08:39:41 +0200

    fix: lhci app

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -3,7 +3,6 @@ const withNx = require("@nrwl/next/plugins/with-nx")
 
 module.exports = withNx({
   webpack: (config, { isServer }) => {
-    console.log(JSON.stringify(config))
     //  Fixes npm packages that depend on `fs` module
     if (!isServer) {
       config.resolve.fallback.fs = false
