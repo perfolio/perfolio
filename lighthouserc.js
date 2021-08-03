b96aae9e0a6fbbac474aeebed50d120f7f7d5632
Andreas Thomas <dev@chronark.com>
2021-08-03 09:36:13 +0200

    fix: lhci app

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,6 +8,8 @@ module.exports = {
         "categories:accessibility": ["error", { minScore: 0.5 }],
         "categories:best-practices": ["error", { minScore: 0.7 }],
         "categories:seo": ["error", { minScore: 0.7 }],
+
+        "first-contentful-paint": ["error", { minScore: 0.6 }],
       },
     },
   },
