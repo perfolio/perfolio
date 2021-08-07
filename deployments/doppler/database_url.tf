3573f5e88ad61fb14166159fccfc52e42c231e67
Andreas Thomas <dev@chronark.com>
2021-08-07 20:59:11 +0200

    k6 (#361)
    * feat: prevent user from adding assets at invalid exchanges
    
    * ci: fix terraform
    
    * fix: set schema to public
    
    * ci: remove cypress
    
    * fix: add pgbouncer during runtime
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools
    
    * feat: rename pools

--- deployments/doppler/database_url.tf
+++ deployments/doppler/database_url.tf
@@ -1,25 +1,37 @@
-
-resource "doppler_secret" "database_url_prod" {
+resource "doppler_secret" "database_url_pool_prod" {
   project = "api-perfolio"
   config  = "prod"
   name    = "DATABASE_URL"
-  value   = var.database_urls.prod
+  value   = var.database_urls.prod_pool
 }
-resource "doppler_secret" "database_url_staging" {
+resource "doppler_secret" "database_url_pool_staging" {
   project = "api-perfolio"
   config  = "preview"
   name    = "DATABASE_URL"
-  value   = var.database_urls.staging
+  value   = var.database_urls.staging_pool
 }
-resource "doppler_secret" "database_url_dev" {
+resource "doppler_secret" "database_url_pool_dev" {
   project = "api-perfolio"
   config  = "dev"
   name    = "DATABASE_URL"
-  value   = var.database_urls.dev
+  value   = var.database_urls.dev_pool
 }
 resource "doppler_secret" "database_url_shadow" {
   project = "api-perfolio"
   config  = "dev"
   name    = "SHADOW_DATABASE_URL"
   value   = var.database_urls.shadow
+}
+resource "doppler_secret" "database_url_direct_prod" {
+  project = "api-perfolio"
+  config  = "prod"
+  name    = "DATABASE_URL_DIRECT"
+  value   = var.database_urls.direct
+}
+
+resource "doppler_secret" "database_url_direct_staging" {
+  project = "api-perfolio"
+  config  = "staging"
+  name    = "DATABASE_URL_DIRECT"
+  value   = var.database_urls.direct
 }
