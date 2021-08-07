5f103c13059ae24b390e7c5b896411476b410cc6
Andreas Thomas <dev@chronark.com>
2021-08-07 21:42:15 +0200

    pool (#364)
    * feat: prevent user from adding assets at invalid exchanges
    
    * ci: fix terraform
    
    * fix: set schema to public
    
    * ci: remove cypress
    
    * fix: add pgbouncer during runtime
    
    * fix: pools
    
    * fix: pools
    
    * fix: pools
    
    * fix: pools

--- scripts/build-api.bash
+++ scripts/build-api.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 npx nx build api --prod --outputPath=.
-npx prisma migrate deploy --schema=libs/integrations/prisma/schema.prisma
+DATABASE_URL=$DATABASE_URL_DIRECT npx prisma migrate deploy --schema=libs/integrations/prisma/schema.prisma
