407ab52b3c68c5690a7ca7c5e87e0eb8f839b415
Andreas Thomas <dev@chronark.com>
2021-08-07 20:42:22 +0200

    feat: rename pools

--- deployments/digitalocean/output.tf
+++ deployments/digitalocean/output.tf
@@ -7,13 +7,13 @@ output "database_url_prod_pool" {
 
 
 
-output "database_url_staging" {
+output "database_url_staging_pool" {
   value = digitalocean_database_connection_pool.staging_pool.uri
 }
 
-output "database_url_dev" {
+output "database_url_dev_pool" {
   value = digitalocean_database_connection_pool.dev_pool.uri
 }
-output "database_url_shadow" {
+output "database_url_shadow_pool" {
   value = digitalocean_database_connection_pool.shadow_pool.uri
 }
