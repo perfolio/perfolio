042b5d5311237821aa5a6fadeedfbb67c5376f94
Andreas Thomas <dev@chronark.com>
2021-08-07 19:16:04 +0200

    pgbouncer (#359)
    * feat: prevent user from adding assets at invalid exchanges
    
    * ci: fix terraform
    
    * fix: set schema to public
    
    * ci: remove cypress
    
    * fix: add pgbouncer during runtime

--- libs/integrations/prisma/src/index.ts
+++ libs/integrations/prisma/src/index.ts
@@ -1,1 +1,15 @@
-export * from "@prisma/client"
+import { PrismaClient as BaseClient } from "@prisma/client"
+export type { StockMap, Transaction, User, UserSettings, Currency } from "@prisma/client"
+
+/**
+ * Inject the `pgbouncer=true` flag into the url.
+ * This is done here because we require the url without pgbouncer for the migration
+ * which takes place during the build step
+ */
+export class PrismaClient extends BaseClient {
+  constructor() {
+    const url = process.env["DATABASE_URL"]
+
+    super({ datasources: { db: { url: `${url}&pgbouncer=true` } } })
+  }
+}
