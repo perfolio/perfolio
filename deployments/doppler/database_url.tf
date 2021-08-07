e6112ba2f28a8ce6289f79562ae8a88858c06605
Andreas Thomas <dev@chronark.com>
2021-08-07 19:08:27 +0200

    fix: add pgbouncer during runtime

--- deployments/doppler/database_url.tf
+++ deployments/doppler/database_url.tf
@@ -3,23 +3,23 @@ resource "doppler_secret" "database_url_prod" {
   project = "api-perfolio"
   config  = "prod"
   name    = "DATABASE_URL"
-  value   = "${var.database_urls.prod}&pgbouncer=true"
+  value   = var.database_urls.prod
 }
 resource "doppler_secret" "database_url_staging" {
   project = "api-perfolio"
   config  = "preview"
   name    = "DATABASE_URL"
-  value   = "${var.database_urls.staging}&pgbouncer=true"
+  value   = var.database_urls.staging
 }
 resource "doppler_secret" "database_url_dev" {
   project = "api-perfolio"
   config  = "dev"
   name    = "DATABASE_URL"
-  value   = "${var.database_urls.dev}&pgbouncer=true"
+  value   = var.database_urls.dev
 }
 resource "doppler_secret" "database_url_shadow" {
   project = "api-perfolio"
   config  = "dev"
   name    = "SHADOW_DATABASE_URL"
-  value   = "${var.database_urls.shadow}&pgbouncer=true"
+  value   = var.database_urls.shadow
 }
