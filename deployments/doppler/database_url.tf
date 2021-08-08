553840e41692b8a09756196637da3f9e412419c4
Andreas Thomas <dev@chronark.com>
2021-08-08 08:42:11 +0200

    feat: separate pool names

--- deployments/doppler/database_url.tf
+++ deployments/doppler/database_url.tf
@@ -1,36 +1,47 @@
-resource "doppler_secret" "database_url_prod_pool" {
-  project = "api-perfolio"
-  config  = "prod"
-  name    = "DATABASE_URL"
-  value   = "${var.database_urls.prod_pool}&pgbouncer=true"
-}
-resource "doppler_secret" "database_url_staging_pool" {
-  project = "api-perfolio"
-  config  = "preview"
-  name    = "DATABASE_URL"
-  value   = "${var.database_urls.staging_pool}&pgbouncer=true"
-}
-resource "doppler_secret" "database_url_dev_pool" {
+
+
+resource "doppler_secret" "database_url_dev" {
   project = "api-perfolio"
   config  = "dev"
   name    = "DATABASE_URL"
-  value   = "${var.database_urls.dev_pool}&pgbouncer=true"
+  value   = var.database_urls.dev
 }
+resource "doppler_secret" "database_url_preview" {
+  project = "api-perfolio"
+  config  = "preview"
+  name    = "DATABASE_URL"
+  value   = var.database_urls.preview
+}
+resource "doppler_secret" "database_url_prod" {
+  project = "api-perfolio"
+  config  = "prod"
+  name    = "DATABASE_URL"
+  value   = var.database_urls.prod
+}
+
 resource "doppler_secret" "database_url_shadow" {
   project = "api-perfolio"
   config  = "dev"
   name    = "SHADOW_DATABASE_URL"
   value   = var.database_urls.shadow
 }
-resource "doppler_secret" "database_url_prod" {
+
+
+resource "doppler_secret" "database_url_dev_pool" {
   project = "api-perfolio"
-  config  = "prod"
-  name    = "DATABASE_URL_DIRECT"
-  value   = var.database_urls.prod
+  config  = "dev"
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.dev_pool
 }
-resource "doppler_secret" "database_url_staging" {
+resource "doppler_secret" "database_url_preview_pool" {
   project = "api-perfolio"
   config  = "preview"
-  name    = "DATABASE_URL_DIRECT"
-  value   = var.database_urls.staging
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.preview_pool
+}
+resource "doppler_secret" "database_url_prod_pool" {
+  project = "api-perfolio"
+  config  = "prod"
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.prod_pool
 }
