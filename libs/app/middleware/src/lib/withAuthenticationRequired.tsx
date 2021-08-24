712dff9db905d917d13bb4a8b6603a8f70b1c756
Andreas Thomas <dev@chronark.com>
2021-08-25 00:19:41 +0200

    auth3 (#380)
    * fix: infinite auth loop
    
    * fix: infinite auth loop
    
    * fix: infinite auth loop

--- libs/app/middleware/src/lib/withAuthenticationRequired.tsx
+++ libs/app/middleware/src/lib/withAuthenticationRequired.tsx
@@ -102,7 +102,6 @@ export function withAuthenticationRequired(
     if (isLoading) {
       return <FullscreenSpinner />
     }
-    console.log({ accessToken, isLoading })
     if (!accessToken) {
       window.location.assign(opts?.returnTo ?? "/auth/sign-in")
     }
