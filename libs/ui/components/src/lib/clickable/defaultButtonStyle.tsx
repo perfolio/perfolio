9b26759f2c0a7e4137ca23abf29017f5fbf0a79d
Andreas Thomas <dev@chronark.com>
2021-08-25 00:14:11 +0200

    fix: infinite auth loop

--- libs/ui/components/src/lib/clickable/defaultButtonStyle.tsx
+++ libs/ui/components/src/lib/clickable/defaultButtonStyle.tsx
@@ -37,6 +37,7 @@ export const DefaultButtonStyle: React.FC<DefaultButtonStyleProps> = ({
         rounded
         items-center
         whitespace-nowrap
+        
         justify-${justify}
         ${shadow(kind, size)}
         ${colors(kind)}
@@ -104,6 +105,9 @@ const text = (size: Size): string => {
 const shadow = (kind: Kind, size: Size): string => {
   if (kind === "plain") {
     return ""
+  }
+  if (kind === "cta") {
+    return "shadow-cta hover:shadow-lg"
   }
 
   return {
