8a5d760ed5e1e3730b4bdaf36b87a5c2df71b69e
Andreas Thomas <dev@chronark.com>
2021-08-25 18:54:52 +0200

    fix: sendgrid cnames (#381)
    * fix: sendgrid cnames
    
    * fix: invalid name

--- deployments/cloudflare/sendgrid.tf
+++ deployments/cloudflare/sendgrid.tf
@@ -1,27 +1,27 @@
-resource "cloudflare_record" "url6754-perfol-io" {
-  name    = "url6754.perfol.io"
+resource "cloudflare_record" "url801-perfol-io" {
+  name    = "url801.perfol.io"
   value   = "sendgrid.net"
   type    = "CNAME"
   zone_id = var.config.zone_id
 }
 
-resource "cloudflare_record" "A22268761-perfol-io" {
-  name    = "22268761.perfol.io"
+resource "cloudflare_record" "_23043673-perfol-io" {
+  name    = "23043673.perfol.io"
   value   = "sendgrid.net"
   type    = "CNAME"
   zone_id = var.config.zone_id
 }
 
-resource "cloudflare_record" "em1023-perfol-io" {
-  name    = "em1023.perfol.io"
-  value   = "u22268761.wl012.sendgrid.net"
+resource "cloudflare_record" "em7361-perfol-io" {
+  name    = "em7361.perfol.io"
+  value   = "u23043673.wl174.sendgrid.net"
   type    = "CNAME"
   zone_id = var.config.zone_id
 }
 
 resource "cloudflare_record" "s1-_domainkey-perfol-io" {
   name    = "s1._domainkey.perfol.io"
-  value   = "s1.domainkey.u22268761.wl012.sendgrid.net"
+  value   = "s1.domainkey.u23043673.wl174.sendgrid.net"
   type    = "CNAME"
   zone_id = var.config.zone_id
 }
@@ -29,7 +29,7 @@ resource "cloudflare_record" "s1-_domainkey-perfol-io" {
 
 resource "cloudflare_record" "s2-_domainkey-perfol-io" {
   name    = "s2._domainkey.perfol.io"
-  value   = "s2.domainkey.u22268761.wl012.sendgrid.net"
+  value   = "s2.domainkey.u23043673.wl174.sendgrid.net"
   type    = "CNAME"
   zone_id = var.config.zone_id
 }
