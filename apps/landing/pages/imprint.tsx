4b99622cf94500144e9c288e06b1d4d7a137edbf
Andreas Thomas <dev@chronark.com>
2021-08-25 21:39:09 +0200

    imprint (#383)
    * feat: send mails from noreply@perfol.io
    
    * feat: add new imprint
    
    * fix: remove trailing slash

--- apps/landing/pages/imprint.tsx
+++ apps/landing/pages/imprint.tsx
@@ -10,7 +10,7 @@ export const Imprint: NextPage = () => {
           <Navbar />
         </div>
         <Section className="mx-auto prose-sm prose sm:prose lg:prose-lg xl:prose-xl">
-          <span className="text-sm ">German only</span>
+          <span>German only</span>
           <h1>Imprint / Impressum</h1>
           <h2>Angaben gemäß § 5 TMG</h2>
           <span>Jordt Kohler Mackintosh Meister Meister Thomas Webersinke GbR</span>
@@ -18,6 +18,11 @@ export const Imprint: NextPage = () => {
           <span>Treibberg 9</span>
           <br></br>
           <span>90403 Nürnberg</span>
+          <div className="flex flex-col">
+            <span>Web: https://perfol.io</span>
+            <span>Telefon: +49 (0) 160 155 1312</span>
+            <span>E-Mail: info@perfol.io</span>
+          </div>
           <h2>Vertreten durch:</h2>
           <div className="flex flex-col">
             <span>Mads Jordt</span>
@@ -28,72 +33,37 @@ export const Imprint: NextPage = () => {
             <span>Andreas Thomas</span>
             <span>Nicolas Webersinke</span>
           </div>
-          <h2>Kontakt</h2>
+
+          <h2>Online-Streitbeilegung (Art. 14 Abs. 1 ODR-Verordnung)</h2>
+          <p>
+            Die Europäische Kommission stellt unter{" "}
+            <a href="ec.europa.eu/consumers/odr">ec.europa.eu/consumers/odr</a> eine Plattform zur
+            Online-Streitbeilegung bereit.
+          </p>
+
+          <h2>Verantwortliche im Sinne von § 18 Abs. 2 MStV</h2>
           <div className="flex flex-col">
-            <span>Telefon: +49 (0) 160 155 1312</span>
-            <span>E-Mail: info@perfol.io</span>
+            <span>Lauren Mackintosh</span>
+            <span>Marketing & Sales</span>
+            <span>Treibberg 9</span>
+            <span>90403 Nürnberg</span>
           </div>
 
-          <h2> EU-Streitschlichtung</h2>
+          <h2>Social Media</h2>
           <p>
-            Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
-            https://ec.europa.eu/consumers/odr. Unsere E-Mail-Adresse finden Sie oben im Impressum.
-          </p>
-
-          <h2>Verbraucherstreitbeilegung / Universalschlichtungsstelle</h2>
-          <p>
-            Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
-            Verbraucherschlichtungsstelle teilzunehmen.
-          </p>
-
-          <h2>Haftung für Inhalte</h2>
-          <p>
-            Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten
-            nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
-            Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
-            Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
-            Tätigkeit hinweisen.
-          </p>
-          <p>
-            Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
-            allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch
-            erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
-            Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
-            entfernen.
-          </p>
-          <h2>Haftung für Links</h2>
-
-          <p>
-            Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
-            Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr
-            übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder
-            Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
-            Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum
-            Zeitpunkt der Verlinkung nicht erkennbar.
-          </p>
-          <p>
-            Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
-            Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
-            Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-          </p>
-          <h2>Urheberrecht</h2>
-          <p>
-            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen
-            dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art
-            der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen
-            Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind
-            nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-          </p>
-          <p>
-            Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die
-            Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche
-            gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden,
-            bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
-            werden wir derartige Inhalte umgehend entfernen.
-          </p>
-
-          <p>
-            Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a>
+            Dieses Impressum gilt auch für unsere Social-Media-Profile auf
+            <br />
+            <span>
+              Instagram:{" "}
+              <a href="https://www.instagram.com/perfol.io">https://www.instagram.com/perfol.io</a>
+            </span>
+            <br />
+            <span>
+              Linkedin:{" "}
+              <a href="https://www.linkedin.com/company/perfolio">
+                https://www.linkedin.com/company/perfolio
+              </a>
+            </span>
           </p>
         </Section>
       </div>
