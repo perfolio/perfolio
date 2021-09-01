bbb6ff3250d9b083ab069d9043bb617fa1f8feb2
Andreas Thomas <dev@chronark.com>
2021-09-01 19:01:26 +0200

    feat: debug

--- apps/app/pages/api/stripe/webhook/subscriptions.ts
+++ apps/app/pages/api/stripe/webhook/subscriptions.ts
@@ -93,7 +93,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (!role) {
       throw new Error(`Product ${product.name} is missing the "authRole" metadata`)
     }
-
     logger.debug(await auth0.getUsers())
 
     switch (event.type) {
