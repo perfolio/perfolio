5605518642e29da0fe767be0a952a83c497e7be5
Andreas Thomas <dev@chronark.com>
2021-09-01 22:02:33 +0200

    on login (#415)
    * fix: rename api route
    
    * fix: rename api route
    
    * fix: multiple res.write
    
    * fix: multiple res.write
    
    * feat: add logging to webhooks
    
    * feat: debug
    
    * feat: debug
    
    * feat: debug

--- apps/app/pages/api/auth0/register-user.ts
+++ apps/app/pages/api/auth0/register-user.ts
@@ -12,7 +12,7 @@ import { z } from "zod"
 import { Logger } from "tslog"
 import { PrismaClient } from "@perfolio/integrations/prisma"
 import { HTTPError } from "@perfolio/util/errors"
-
+import { ManagementClient } from "auth0"
 const validation = z.object({
   headers: z.object({
     "content-type": z.string().refine((h) => h === "application/json"),
@@ -25,8 +25,18 @@ const validation = z.object({
   method: z.string().refine((m) => m === "POST"),
 })
 
+const auth0 = new ManagementClient({
+  domain: env.require("NEXT_PUBLIC_AUTH0_DOMAIN"),
+  clientId: env.require("AUTH0_MANAGEMENT_CLIENT_ID"),
+  clientSecret: env.require("AUTH0_MANAGEMENT_CLIENT_SECRET"),
+})
 const prisma = new PrismaClient()
 
+/**
+ * This webhook gets called by auth0 only on the first login of a user
+ * It creates a new stripe customer, database entry and subscription.
+ * And sets the access role in auth0
+ */
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const logger = new Logger({ name: "on registration" })
   try {
@@ -40,16 +50,42 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       typescript: true,
     })
 
-    const customer = await stripe.customers.create({ email })
-
-    const user = await prisma.user.create({
-      data: {
-        id: userId,
-        email,
-        stripeCustomerId: customer.id,
-      },
+    const customer = await stripe.customers.create({ email }).catch((err) => {
+      throw new HTTPError(500, `Unable to create customer in stripe ${email}: ${err}`)
     })
-    logger.debug("Created new user", user)
+
+    const user = await prisma.user
+      .create({
+        data: {
+          id: userId,
+          email,
+          stripeCustomerId: customer.id,
+        },
+      })
+      .catch((err) => {
+        throw new HTTPError(500, `Unable to create user in database: ${err}`)
+      })
+
+    await stripe.subscriptions
+      .create({
+        customer: user.stripeCustomerId,
+        trial_period_days: 7,
+        items: [
+          {
+            // Growth subscription
+            price: "price_1JU4LpG0ZLpKb1P6Szj2jJQr",
+          },
+        ],
+      })
+      .catch((err) => {
+        throw new HTTPError(500, `Unable to create subscription for user ${userId}: ${err}`)
+      })
+
+    const growthRole = "rol_Rjy99HLtin8ryEds"
+    await auth0.assignRolestoUser({ id: user.id }, { roles: [growthRole] }).catch((err) => {
+      throw new HTTPError(500, `Unable to assign role ${growthRole} to user ${user.id}: ${err}`)
+    })
+
     res.json({ received: true })
   } catch (err) {
     res.status(err instanceof HTTPError ? err.status : 500)
