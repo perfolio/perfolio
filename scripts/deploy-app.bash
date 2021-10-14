1836e0702fdac49d546cbc96a9d1e3fd5b00a760
Andreas Thomas <dev@chronark.com>
2021-10-14 22:24:44 +0200

    build(i18n): copy locale files during build

--- scripts/deploy-app.bash
+++ scripts/deploy-app.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 npx nx build app --prod
+# copy locales over
+cp  ./libs/feature/i18n/src/lib/locales/**/*.json --parents ./dist/apps/app
+
 npx prisma migrate deploy --schema=libs/integrations/prisma/schema.prisma
