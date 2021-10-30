1263f22bdf1ea4c74b87d9aa3aa441ae3a8c963c
Andreas Thomas <dev@chronark.com>
2021-10-30 18:29:04 +0200

    locale (#464)
    * fix: downgrade to next11
    
    * fix: downgrade to next11
    
    * fix: downgrade to next11

--- pkg/i18n/translate.ts
+++ pkg/i18n/translate.ts
@@ -1,5 +1,6 @@
-import { resolve } from "path"
+import { join, resolve } from "path"
 import fs from "fs"
+import { env } from "@chronark/env"
 
 type Namespace = "landing" | "app"
 type Locale = "en" | "de"
@@ -8,7 +9,24 @@ type Locale = "en" | "de"
  * Load a translations file from local fs
  */
 const loadTranslation = (namespace: Namespace, locale: Locale): Promise<Record<string, string>> => {
-  const path = resolve("public", "locales", locale, `${namespace}.json`)
+  const relativePath = ["pkg", "i18n", "locales", locale, `${namespace}.json`]
+  let path: string
+
+  switch (env.get("VERCEL_ENV")) {
+    case "production":
+    case "preview":
+      path = join(
+        "https://raw.githubusercontent.com/perfolio/perfolio",
+        env.require("VERCEL_GIT_COMMIT_REF"),
+        ...relativePath,
+      )
+      break
+
+    default:
+      path = resolve(...relativePath)
+      break
+  }
+
   try {
     return JSON.parse(fs.readFileSync(path).toString())
   } catch (err) {
