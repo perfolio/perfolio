73577804b864b03e2ccd4e5ee17b5cd97d28a165
Andreas Thomas <dev@chronark.com>
2021-11-07 14:41:45 +0100

    fix: vercel secret

--- deployments/vercel/database_url.tf
+++ deployments/vercel/database_url.tf
@@ -2,9 +2,9 @@
 
 
 resource "vercel_secret" "database_url_dev" {
-  team_id    = var.team_id
-  name       = "DATABASE_URL"
-  value      = var.database_urls.dev
+  team_id = var.team_id
+  name    = "DATABASE_URL"
+  value   = var.database_urls.dev
 }
 resource "vercel_env" "database_url_dev" {
   project_id = vercel_project.perfolio.id
@@ -16,9 +16,9 @@ resource "vercel_env" "database_url_dev" {
 
 
 resource "vercel_secret" "database_url_preview" {
-  team_id    = var.team_id
-  name       = "DATABASE_URL"
-  value      = var.database_urls.preview
+  team_id = var.team_id
+  name    = "DATABASE_URL"
+  value   = var.database_urls.preview
 }
 resource "vercel_env" "database_url_preview" {
   project_id = vercel_project.perfolio.id
@@ -29,9 +29,9 @@ resource "vercel_env" "database_url_preview" {
 }
 
 resource "vercel_secret" "database_url_production" {
-  team_id    = var.team_id
-  name       = "DATABASE_URL"
-  value      = var.database_urls.production
+  team_id = var.team_id
+  name    = "DATABASE_URL"
+  value   = var.database_urls.prod
 }
 resource "vercel_env" "database_url_production" {
   project_id = vercel_project.perfolio.id
@@ -39,4 +39,48 @@ resource "vercel_env" "database_url_production" {
   type       = "secret"
   key        = "DATABASE_URL"
   value      = vercel_secret.database_url_production.id
+}
+
+
+
+
+
+resource "vercel_secret" "database_url_dev_pool" {
+  team_id = var.team_id
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.dev_pool
+}
+resource "vercel_env" "database_url_dev_pool" {
+  project_id = vercel_project.perfolio.id
+  target     = ["development"]
+  type       = "secret"
+  key        = "DATABASE_URL_POOL"
+  value      = vercel_secret.database_url_dev_pool.id
+}
+
+
+resource "vercel_secret" "database_url_preview_pool" {
+  team_id = var.team_id
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.preview_pool
+}
+resource "vercel_env" "database_url_preview_pool" {
+  project_id = vercel_project.perfolio.id
+  target     = ["preview"]
+  type       = "secret"
+  key        = "DATABASE_URL_POOL"
+  value      = vercel_secret.database_url_preview_pool.id
+}
+
+resource "vercel_secret" "database_url_production_pool" {
+  team_id = var.team_id
+  name    = "DATABASE_URL_POOL"
+  value   = var.database_urls.prod_pool
+}
+resource "vercel_env" "database_url_production_pool" {
+  project_id = vercel_project.perfolio.id
+  target     = ["production"]
+  type       = "secret"
+  key        = "DATABASE_URL_POOL"
+  value      = vercel_secret.database_url_production_pool.id
 }
