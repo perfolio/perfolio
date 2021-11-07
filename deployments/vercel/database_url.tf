e8f6bb5758d37c6a56003ac6233c9092a2d7180e
Andreas Thomas <dev@chronark.com>
2021-11-07 15:02:39 +0100

    ci: tf

--- deployments/vercel/database_url.tf
+++ deployments/vercel/database_url.tf
@@ -3,7 +3,7 @@
 
 resource "vercel_secret" "database_url_dev" {
   team_id = var.team_id
-  name    = "DATABASE_URL"
+  name    = "database_url_dev"
   value   = var.database_urls.dev
 }
 resource "vercel_env" "database_url_dev" {
@@ -17,7 +17,7 @@ resource "vercel_env" "database_url_dev" {
 
 resource "vercel_secret" "database_url_preview" {
   team_id = var.team_id
-  name    = "DATABASE_URL"
+  name    = "database_url_preview"
   value   = var.database_urls.preview
 }
 resource "vercel_env" "database_url_preview" {
@@ -30,7 +30,7 @@ resource "vercel_env" "database_url_preview" {
 
 resource "vercel_secret" "database_url_production" {
   team_id = var.team_id
-  name    = "DATABASE_URL"
+  name    = "database_url_production"
   value   = var.database_urls.prod
 }
 resource "vercel_env" "database_url_production" {
@@ -47,7 +47,7 @@ resource "vercel_env" "database_url_production" {
 
 resource "vercel_secret" "database_url_dev_pool" {
   team_id = var.team_id
-  name    = "DATABASE_URL_POOL"
+  name    = "database_url_dev_pool"
   value   = var.database_urls.dev_pool
 }
 resource "vercel_env" "database_url_dev_pool" {
@@ -61,7 +61,7 @@ resource "vercel_env" "database_url_dev_pool" {
 
 resource "vercel_secret" "database_url_preview_pool" {
   team_id = var.team_id
-  name    = "DATABASE_URL_POOL"
+  name    = "database_url_preview_pool"
   value   = var.database_urls.preview_pool
 }
 resource "vercel_env" "database_url_preview_pool" {
@@ -74,7 +74,7 @@ resource "vercel_env" "database_url_preview_pool" {
 
 resource "vercel_secret" "database_url_production_pool" {
   team_id = var.team_id
-  name    = "DATABASE_URL_POOL"
+  name    = "database_url_production_pool"
   value   = var.database_urls.prod_pool
 }
 resource "vercel_env" "database_url_production_pool" {
