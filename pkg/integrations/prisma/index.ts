5aa305079c26679404c9608f79280dfe6d795144
Andreas Thomas <dev@chronark.com>
2021-11-12 22:22:16 +0100

    fix: use DATABASE_URL

--- pkg/integrations/prisma/index.ts
+++ pkg/integrations/prisma/index.ts
@@ -1,28 +1,2 @@
-import { PrismaClient as BaseClient } from "@prisma/client"
-import { env } from "@chronark/env"
+export { PrismaClient } from "@prisma/client"
 export type { StockMap, Transaction, User, Settings, Portfolio, Currency } from "@prisma/client"
-
-const url = (): string => {
-  const username = env.require("PLANETSCALE_DB_USERNAME")
-  const password = env.require("PLANETSCALE_DB_PASSWORD")
-  const host = env.require("PLANETSCALE_DB_HOST")
-  const db = env.require("PLANETSCALE_DB")
-  return `mysql://${username}:${password}@${host}/${db}?sslaccept=strict`
-}
-
-/**
- * Inject the `pgbouncer=true` flag into the url.
- * This is done here because we require the url without pgbouncer for the migration
- * which takes place during the build step
- */
-export class PrismaClient extends BaseClient {
-  constructor() {
-    super({
-      datasources: {
-        db: {
-          url: url(),
-        },
-      },
-    })
-  }
-}
