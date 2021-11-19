a4d22ca46d756e45601dd267eb2afb39f8ba1039
SaitaoGit <32571437+SaitaoGit@users.noreply.github.com>
2021-11-19 21:49:10 +0100

    feat: add member in imprint (#494)
    * feat: add member in imprint
    
    * feat: change order to alphabetic

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -32,6 +32,7 @@ export const Imprint: NextPage<ImprintPageProps> = ({ translations }) => {
           </div>
           <h2>Vertreten durch:</h2>
           <div className="flex flex-col">
+            <span>Tobias Hassel</span>
             <span>Mads Jordt</span>
             <span>Kevin Kohler</span>
             <span>Luis Meister</span>
