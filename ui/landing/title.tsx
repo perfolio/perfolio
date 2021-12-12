43394bfb5ea462211d963848c8df142d2c47b574
Tobias Hassel <tobias.hassel1@web.de>
2021-12-12 12:31:26 +0100

    fix: revert title changes

--- ui/landing/title.tsx
+++ ui/landing/title.tsx
@@ -1,23 +1,13 @@
-import { Transition } from "@headlessui/react"
-import React, { useRef } from "react"
-import { useInViewport } from "react-in-viewport"
+import React from "react"
 
 export interface SectionTitleProps {
   title: string
   tag: string
-  animate?: boolean
 }
 
-export const SectionTitle: React.FC<SectionTitleProps> = ({
-  title,
-  tag,
-  animate = false,
-}): JSX.Element => {
-  const myRef = useRef<HTMLDivElement>(null)
-  const { inViewport, enterCount } = useInViewport(myRef)
-
-  const content = () => {
-    return (
+export const SectionTitle: React.FC<SectionTitleProps> = ({ title, tag }): JSX.Element => {
+  return (
+    <div className="px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8">
       <div className="text-center">
         <h2 className="text-base font-semibold tracking-wide text-transparent uppercase bg-gradient-to-tr bg-clip-text from-primary to-secondary">
           {tag}
@@ -26,24 +16,6 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({
           {title}
         </p>
       </div>
-    )
-  }
-
-  return (
-    <div ref={myRef} className="px-4 py-16 mx-auto max-w-7xl sm:py-24 sm:px-6 lg:px-8">
-      {animate ? (
-        <Transition
-          appear={true}
-          show={inViewport || enterCount >= 1}
-          enter="transform transition ease-in-out duration-500 "
-          enterFrom="-translate-x-full opacity-0"
-          enterTo="translate-x-0 opacity-100"
-        >
-          {content()}
-        </Transition>
-      ) : (
-        <>{content()}</>
-      )}
     </div>
   )
 }
