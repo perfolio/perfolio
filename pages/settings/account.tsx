39c12960c98d2e664f08a4e24cbadc594c3d82f0
Tobias Hassel <tobias.hassel1@web.de>
2022-02-02 18:54:19 +0100

    fix: delete comments

--- pages/settings/account.tsx
+++ pages/settings/account.tsx
@@ -2,38 +2,26 @@ import { GetStaticProps, NextPage } from "next"
 import React from "react"
 import fs from "fs"
 import { AppLayout, SideNavbar } from "@perfolio/ui/app"
-//import { useRouter } from "next/router"
-//import { useUser } from "@perfolio/pkg/hooks"
 import { useI18n } from "next-localization"
 import { SettingCard } from "@perfolio/ui/components/settingcard"
 import { z } from "zod"
-//import { useUpdateSettings } from "@perfolio/pkg/hooks"
 import { CheckIcon } from "@heroicons/react/outline"
 import { useToaster } from "@perfolio/pkg/toaster"
 import { Field } from "@perfolio/ui/form"
 import { HeadingCard } from "@perfolio/ui/components/headingcard"
-/**
- * / page.
- */
 
 interface PageProps {}
 
 const SettingsPage: NextPage<PageProps> = () => {
   const { t } = useI18n()
   const toast = useToaster()
-  // const router = useRouter()
-  // const { user } = useUser()
+
   const languageValidation = z.object({
     defaultLanguage: z.string(),
   })
-  //const updateSettings = useUpdateSettings()
+
   const onLanguageSubmit = async (values: z.infer<typeof languageValidation>): Promise<void> => {
     console.log(values)
-    // const res = await updateSettings.mutateAsync({
-    //   settings: {
-    //     defaultExchangeId: exchanges?.find((e) => e.description === values.defaultExchange)?.mic,
-    //   },
-    // })
     toast.addToast({
       icon: <CheckIcon />,
       title: "Success",
@@ -66,48 +54,6 @@ const SettingsPage: NextPage<PageProps> = () => {
             defaultValue={"en"} // do not hardcode
           />
         </SettingCard>
-
-        {/* <div className="space-y-8">
-        <Button
-          onClick={async () => {
-            if (!user) {
-              return
-            }
-            const res = await fetch(`/api/stripe/create-portal-session/${user?.stripeCustomerId}`, {
-              method: "POST",
-            })
-            if (res.status !== 200) {
-              console.error(res.body)
-            }
-            const { url } = (await res.json()) as { url: string }
-            router.push(url)
-          }}
-          type="cta"
-        >
-          {t("app.setBillingPortal")}
-        </Button>
-        <Button
-          onClick={async () => {
-            if (!user) {
-              return
-            }
-            const res = await fetch(
-              `/api/stripe/checkout/plan_K8CG6n6MPm30fB?customerId=${user.stripeCustomerId}`,
-              {
-                method: "POST",
-              },
-            )
-            if (res.status !== 200) {
-              console.error(res.body)
-            }
-            const { url } = (await res.json()) as { url: string }
-            router.push(url)
-          }}
-          type="cta"
-        >
-          Checkout
-        </Button>
-      </div> */}
       </div>
     </AppLayout>
   )
