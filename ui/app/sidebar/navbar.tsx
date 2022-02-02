24192b07bb232cffaac19fac6da46f69bf51df13
Tobias Hassel <tobias.hassel1@web.de>
2022-02-02 23:12:21 +0100

    feat: small style change to side navigation

--- ui/app/sidebar/navbar.tsx
+++ ui/app/sidebar/navbar.tsx
@@ -23,17 +23,18 @@ export const SideNavbar: React.FC = (): JSX.Element => {
   ]
 
   return (
-    <nav className="mt-8">
-      <ul className="space-y-4">
+    <nav className="mt-8 w-full">
+      <ul className="space-y-2">
         {links.map(({ href, label }) => (
           <li key={label}>
             <Link href={href}>
               <a
                 className={cn(
-                  "block px-4 py-2transition duration-500 ease-in-out focus:outline-none hover:text-black",
+                  "h-10 block px-4 py-2 transition duration-500 ease-in-out focus:outline-none hover:text-black",
                   {
-                    "font-semibold text-black": router.pathname === href,
-                    "text-gray-500": router.pathname !== href,
+                    "font-semibold text-black border-l-4 border-primary bg-gray-100":
+                      router.pathname === href,
+                    "text-gray-500 hover:bg-gray-50 hover:text-black": router.pathname !== href,
                   },
                 )}
               >
