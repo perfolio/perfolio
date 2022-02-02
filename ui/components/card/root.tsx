6656a675a0a24df58e9d783840806367921b0254
Tobias Hassel <tobias.hassel1@web.de>
2022-02-02 21:49:13 +0100

    fix: visual border corner bug in card

--- ui/components/card/root.tsx
+++ ui/components/card/root.tsx
@@ -9,7 +9,7 @@ export interface RootProps {
 export const Root: React.FC<RootProps> = ({ border = true, children }): JSX.Element => {
   return (
     <div
-      className={cn("w-full space-y-4 sm:space-y-8 bg-white rounded", {
+      className={cn("w-full space-y-4 sm:space-y-8 bg-white rounded overflow-hidden", {
         "border border-gray-300": border === true,
       })}
     >
