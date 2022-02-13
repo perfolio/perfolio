319ece5479e2e7a6654c0a8d04792503ea8702d7
Tobias Hassel <tobias.hassel1@web.de>
2022-02-13 23:30:15 +0100

    fix: fix #525 show portfolios total value on dashboard overview

--- pkg/hooks/queries/useCurrentAbsoluteValue.ts
+++ pkg/hooks/queries/useCurrentAbsoluteValue.ts
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router"
 import { useCurrentPorfolioState } from "./useCurrentPortfolioState"
 
-export const useCurrentAbsoluteValue = () => {
-  const { currentPorfolioState, ...meta } = useCurrentPorfolioState()
+export const useCurrentAbsoluteValue = (opts?: { portfolioId?: string; since?: number }) => {
+  const router = useRouter()
+  const portfolioIdToUse = opts?.portfolioId ?? (router.query["portfolioId"] as string)
+  const { currentPorfolioState, ...meta } = useCurrentPorfolioState({
+    portfolioId: portfolioIdToUse,
+  })
   let currentAbsoluteValue = 0
   if (currentPorfolioState) {
     Object.values(currentPorfolioState).forEach((e) => {
--- pkg/hooks/queries/useCurrentPortfolioState.ts
+++ pkg/hooks/queries/useCurrentPortfolioState.ts
@@ -1,8 +1,11 @@
 import { ValueAndQuantityAtTime } from "@perfolio/pkg/api"
+import { useRouter } from "next/router"
 import { usePortfolioHistory } from ".."
 
-export const useCurrentPorfolioState = () => {
-  const { history, ...meta } = usePortfolioHistory()
+export const useCurrentPorfolioState = (opts?: { portfolioId?: string; since?: number }) => {
+  const router = useRouter()
+  const portfolioIdToUse = opts?.portfolioId ?? (router.query["portfolioId"] as string)
+  const { history, ...meta } = usePortfolioHistory({ portfolioId: portfolioIdToUse })
   const getLastValid = (history: ValueAndQuantityAtTime[]): { quantity: number; value: number } => {
     const sorted = [...history].sort((a, b) => b.time - a.time)
     for (const day of sorted) {
